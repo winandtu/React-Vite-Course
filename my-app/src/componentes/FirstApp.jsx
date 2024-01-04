40ed05507b940e62f9be7eaf61ebbc238670efcc
Danilo Arévalo <jyfy121@gmail.com>
2024-01-04 17:16:11 -0500

    PropTypes
    Colocar tipos a las propiedades de los componentes

--- my-app/src/componentes/FirstApp.jsx
+++ my-app/src/componentes/FirstApp.jsx
@@ -1,17 +1,31 @@
 //import { Fragment } from "react"
 //en lugar de importar Fragment usar <> </> que es lo mismo
 
+//Usar tipos en los componentes con proptypes
+import PropTypes from 'prop-types'
 const suma =(a,b)=> a+b;
-export function FirstApp(){
 
+export function FirstApp(props){
+
+    const {title, subtitle}= props;
     
     return (
         <>
         <h1>Primera aplicación</h1>
         <p>Estudiante de Ingeniería</p>
         <p>{suma(2,5)}</p>
+        <h2>{title}</h2>
+        <p>{subtitle}</p>
         </>
         
         )
+
     
 }
+
+ //Creando los tipos para cada propiedad
+
+ FirstApp.propTypes={
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.number.isRequired
+ }
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //importo mis componentes
+import './styles.css'
 
 import { FirstApp } from './componentes/FirstApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp />
+    <FirstApp title='Hola soy Goku' subtitle={135} />
   </React.StrictMode>,
 )
